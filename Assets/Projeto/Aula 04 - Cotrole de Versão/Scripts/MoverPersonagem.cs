57f85c151736bef33213274b9826d5dbd63480aa
Daniel de Oliveira <lacrimaopera@gmail.com>
2017-03-08 22:04:13 -0300

    Atualização código fonte

---
+++ Assets/Projeto/Aula 04 - Cotrole de Versão/Scripts/MoverPersonagem.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoverPersonagem : MonoBehaviour
+{
+
+	public float velocidade = 0.01f;
+	public GameObject saida, chegada;
+	Vector2 direcao;
+
+	// Use this for initialization
+	void Start ()
+	{
+		transform.position = saida.transform.position;
+
+		if (chegada.transform.position.x < transform.position.x) {
+			direcao = Vector2.left;
+			transform.localScale = new Vector3 (-1f, 1f, 1f);
+		} else { 
+			direcao = Vector2.right;
+			transform.localScale = new Vector3 (1f, 1f, 1f);
+		}
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+
+		transform.Translate (direcao * velocidade * Time.deltaTime);
+
+	}
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.tag == "Porta") {
+			Application.LoadLevel (Application.loadedLevel + 1);
+			// Debug.Log ("Chegamos");
+		}
+	}
+}
